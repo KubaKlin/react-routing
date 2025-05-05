3f2c9f799c6e1c675edd2b88c828bc12ee887b9b
Kuba Klin <poczta@kubaklin.pl>
2025-05-05 17:15:33 +0200

    fix: move functions to hook

--- src/components/ArticleForm/ArticleForm.jsx
+++ src/components/ArticleForm/ArticleForm.jsx
@@ -2,46 +2,27 @@ import { Box, Button, TextField } from '@mui/material';
 import useArticleCreate from '../../hooks/useArticleCreate';
 import useArticleEdit from '../../hooks/useArticleEdit';
 import useArticleForm from './useArticleForm';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 export const ArticleForm = ({ isEditing, article }) => {
   const { handleCreate } = useArticleCreate();
   const { handleEdit } = useArticleEdit();
 
-  const onSubmit = async (articleData) => {
-    if (isEditing) {
-      return await handleEdit(article.id, articleData);
-    } else {
-      return await handleCreate(articleData);
-    }
-  };
-
-  const { handleTitleChange, handleContentChange, title, content } =
-    useArticleForm(article?.title || '', article?.content || '');
-  const [isLoading, setIsLoading] = useState(false);
-
-  const navigate = useNavigate();
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setIsLoading(true);
-
-    try {
-      const articleData = { title, content };
-      const articleId = await onSubmit(articleData);
-
-      navigate(`/articles/${articleId}`);
-    } catch (error) {
-      console.error('Error:', error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  const handleBack = () => {
-    navigate('/articles');
-  };
+  const {
+    title,
+    content,
+    isLoading,
+    handleTitleChange,
+    handleContentChange,
+    handleSubmit,
+    handleBack,
+  } = useArticleForm(
+    article?.title || '',
+    article?.content || '',
+    isEditing,
+    article,
+    handleCreate,
+    handleEdit
+  );
 
   return (
     <form onSubmit={handleSubmit}>
--- src/components/ArticleForm/useArticleForm.jsx
+++ src/components/ArticleForm/useArticleForm.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const useArticleForm = (initialTitle = '', initialContent = '') => {
+const useArticleForm = (initialTitle = '', initialContent = '', isEditing = false, article = null, handleCreate, handleEdit) => {
   const [title, setTitle] = useState(initialTitle);
   const [content, setContent] = useState(initialContent);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleTitleChange = (event) => {
     setTitle(event.target.value);
@@ -17,12 +20,42 @@ const useArticleForm = (initialTitle = '', initialContent = '') => {
     setContent(initialContent);
   };
 
+  const onSubmit = async (articleData) => {
+    if (isEditing) {
+      return await handleEdit(article.id, articleData);
+    } else {
+      return await handleCreate(articleData);
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const articleData = { title, content };
+      const articleId = await onSubmit(articleData);
+      navigate(`/articles/${articleId}`);
+    } catch (error) {
+      console.error('Error:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleBack = () => {
+    navigate('/articles');
+  };
+
   return {
     title,
     content,
+    isLoading,
     resetForm,
     handleTitleChange,
     handleContentChange,
+    handleSubmit,
+    handleBack,
   };
 };
 
